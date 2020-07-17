0ccb3809310e2112faa5ccd7161a91901a9f3c5c
Máté Homolya <mate.homolya@gmail.com>
2020-07-17 20:40:24 +0200

    Add job type in JobPosting component

--- jobs-react-app/src/components/JobPosting.tsx
+++ jobs-react-app/src/components/JobPosting.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useJobPosting } from "../lib/hooks";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Link from "@material-ui/core/Link";
@@ -8,16 +9,30 @@ export type JobPostingProps = {
   id: string;
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    boldGreen: {
+      fontWeight: 500,
+      color: theme.palette.success.light,
+    },
+  })
+);
+
 export function JobPosting(props: JobPostingProps) {
   const jobPosting = useJobPosting({ id: props.id });
+  const classes = useStyles();
   if (!jobPosting) return <></>;
 
   const SecondaryText: React.ReactNode = (
     <>
       {jobPosting.company_url ? (
-        <Link color="inherit" href={jobPosting.company_url}>
-          {jobPosting.company}
-        </Link>
+        <>
+          <Link color="inherit" href={jobPosting.company_url}>
+            {jobPosting.company}
+          </Link>
+          {jobPosting.type && ` – `}
+          <span className={classes.boldGreen}>{jobPosting.type}</span>
+        </>
       ) : (
         jobPosting.company
       )}
